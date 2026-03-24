0c48ff2b9959709ab7d30480ba9372494f4b745c
IsmaelG <ismaelgn89@gmail.com>
2026-03-24 10:38:59 +0100

    feature: level-1

---
+++ level-1/index.php
@@ -0,0 +1,67 @@
+<?php
+
+// Exercise 1
+$list = [10,20,30,40,50];
+
+
+foreach ($list as $number) {
+    echo $number . ", ";
+}
+
+echo "<br>" . "-------------" . "<br>" . "<br>";
+
+
+//Exercise 2
+
+$elements = [10,20,30,40,50,60];
+
+echo "Result index count: " . "<br>" . count($elements) . "<br>";
+
+unset($elements[4]);
+
+$elements = array_values($elements);
+
+echo "Result index count after normalize: " . "<br>" . count($elements) . "<br>";
+
+print_r($elements) . "<br>";
+
+
+echo "<br>" . "-------------" . "<br>" . "<br>";
+
+
+//Exercise 3
+
+$lenguages = ["php", "html", "hp"];
+
+function checkWords($lenguages, string $letter)
+{
+
+    foreach ($lenguages as $word) {
+
+        if (!str_contains($word, $letter)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+echo "Lenguages contains h?" . "<br>";
+var_dump(checkWords($lenguages, "h"));
+
+
+echo "<br>" . "-------------" . "<br>" . "<br>";
+
+
+//Exercise 4
+
+$personal = [
+ "name" => "Ismael",
+ "age" => "36",
+ "mail" => "ismaelgn89@gmail.com",
+ "food" => "pasta"
+];
+
+echo $personal["name"] . "<br>";
+echo $personal["age"] . "<br>";
+echo $personal["mail"] . "<br>";
+echo $personal["food"] . "<br>";
